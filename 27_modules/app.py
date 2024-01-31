196a638c2d269ceb6f7c66fdc258c74008c0deac
Galib-23 <aagalib2323@gmail.com>
2024-01-31 20:16:20 +0600

    Modules

---
+++ 27_modules/app.py
@@ -0,0 +1,6 @@
+import modules
+# We can also import a specific function like this:
+# from modules import convert_inch_to_feet
+
+feet = modules.convert_inch_to_feet(12)
+print(feet)
---
+++ 27_modules/modules.py
@@ -0,0 +1,6 @@
+def convert_kg_to_lb(kg):
+    return kg * 2.2
+
+def convert_inch_to_feet(inch):
+    feet = inch / 12
+    return feet
